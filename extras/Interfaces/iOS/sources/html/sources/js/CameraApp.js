0a6a9870d1c309b8c697f38e27dd2d7eb5df9ee6
Landon Harnish <ljharnish@gmail.com>
2025-04-30 17:14:56 -0400

    Old Youtube

--- extras/Interfaces/iOS/sources/html/sources/js/CameraApp.js
+++ extras/Interfaces/iOS/sources/html/sources/js/CameraApp.js
@@ -9,3 +9,15 @@ if (navigator.mediaDevices.getUserMedia) {
       console.log("Something went wrong!");
     });
 }
+
+function takePicture() {
+    let canvas = document.createElement('canvas');
+
+	canvas.height = window.getComputedStyle(document.body).height.replace('px', '');
+	canvas.width = window.getComputedStyle(document.body).width.replace('px', '');
+	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);;
+    let image_data_url = canvas.toDataURL('image/jpeg');
+
+    // data url of the image
+    console.log(image_data_url);
+}
